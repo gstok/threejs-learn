97117b4a3db6bc355459782b4e9f3f727d145b86
顾世豪 <gushihao@worken.cn>
2018-04-24 18:36:52 +0800

    下班提交

--- src/index.js
+++ src/index.js
@@ -14,72 +14,50 @@ let scene, camera, render, asciiRender;
 let stats, gui;
 //场景内的各种对象
 let plane, cube, sphere;
-//
+//场景内的光源
+let spotLight, hemisphereLight, ambientLight;
+//坐标系
+let axes;
+
+
+//datGUI控制的变量
 let controls = new function () {
     this.rotationSpeed = 0.02;
     this.bouncingSpeed = 0.03;
+
+    this.spotX = 0;
+    this.spotY = 400;
+    this.spotZ = 0;
+
+    this.hmspX = 0;
+    this.hmspY = 200;
+    this.hmspZ = 0;
 }
 
-//向场景之中添加需展示的对象
-function addGeometry (scene) {
-    //添加坐标系
-    let axes = new THREE.AxesHelper(50);
-    scene.add(axes);
-
-    //在底部添加一个平面
-    plane = createPlane();
-    scene.add(plane);
-    //添加一个立方体
-    cube = createCube();
-    scene.add(cube);
-    //添加一个球体
-    sphere = createSphere();
-    scene.add(sphere);
-
-    //添加直线光源
+//创建聚光灯光源
+function createSpotLight () {
     let spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(-100, 200, -100);
+    spotLight.position.set(controls.spotX, controls.spotY, controls.spotZ);
     spotLight.castShadow = true;
     spotLight.shadowMapHeight = 1024;
     spotLight.shadowMapWidth = 1024;
-    scene.add(spotLight);
-
-    // let ambientLight = new THREE.AmbientLight(0xffffff);
-    // scene.add(ambientLight);
-
+    return spotLight;
+}
+//创建半球光源
+function createHemisphereLight () {
     let hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x333333, 0.6);
-    hemisphereLight.position.set(0, 200, 0);
-    scene.add(hemisphereLight);
+    hemisphereLight.position.set(controls.hmspX, controls.hmspY, controls.hmspZ);
+    return hemisphereLight;
 }
-
-
-//创建立方体
-function createCube () {
-    let cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
-    let cubeMaterial = new THREE.MeshStandardMaterial({
-        color: 0x8fBc8f
-    });
-    let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.set(10, 10, 10);
-    cube.castShadow = true;
-    return cube; 
-}
-
-//创建球体
-function createSphere () {
-    let sphereGeometry = new THREE.SphereGeometry(10, 20, 20);
-    let sphereMaterial = new THREE.MeshStandardMaterial({
-        color: 0x7777ff
-    });
-    let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    sphere.position.set(30, 30, 30);
-    sphere.castShadow = true;
-    return sphere;
+//创建环境光源
+function createAmbientLight () {
+    let ambientLight = new THREE.AmbientLight("#00ff00");
+    return ambientLight;
 }
 
 //创建面板平面
 function createPlane () {
-    let planeGeometry = new THREE.PlaneGeometry(100, 100, 1, 1);
+    let planeGeometry = new THREE.PlaneGeometry(1000, 1000, 1, 1);
     let planeMaterial = new THREE.MeshStandardMaterial({
         color: 0x333333
     });
@@ -92,13 +70,64 @@ function createPlane () {
     return plane;
 }
 
+//创建摄像机
+function createCamera () {
+    let camera = new THREE.PerspectiveCamera(45, threeArea.clientWidth / threeArea.clientHeight, 0.1, 10000);
+    camera.position.x = 0;
+    camera.position.y = 1000;
+    camera.position.z = 0;
+    return camera;
+}
+
+//创建渲染器
+function createRender () {
+    let render = new THREE.WebGLRenderer();
+    render.setClearColor(0xf0f0f0);
+    render.setSize(threeArea.clientWidth, threeArea.clientHeight);
+    render.gammaInput = true;
+    render.gammaOutput = true;
+    render.shadowMap.enabled = true;
+    render.shadowMapType = THREE.PCFSoftShadowMap;
+    return render;
+}
+
+
+
+//向场景之中添加需展示的对象
+function addGeometry (scene) {
+    //添加坐标系
+    axes = new THREE.AxesHelper(50);
+    scene.add(axes);
+    //在底部添加一个平面
+    plane = createPlane();
+    scene.add(plane);
+    //添加直线光源
+    spotLight = createSpotLight();
+    scene.add(spotLight);
+    //添加半球光源
+    // hemisphereLight = createHemisphereLight();
+    // scene.add(hemisphereLight);
+    //添加环境光源
+    ambientLight = createAmbientLight();
+    scene.add(ambientLight);
+}
+
+
+
+
 //初始化
 function init () {
     stats = initStats();
+
     gui = new dat.GUI();
     gui.add(controls, 'rotationSpeed', 0, 0.2);
     gui.add(controls, 'bouncingSpeed', 0, 0.2);
-
+    gui.add(controls, "spotX", -1000, 1000);
+    gui.add(controls, "spotY", -1000, 1000);
+    gui.add(controls, "spotZ", -1000, 1000);
+    gui.add(controls, "hmspX", -1000, 1000);
+    gui.add(controls, "hmspY", -1000, 1000);
+    gui.add(controls, "hmspZ", -1000, 1000);
 
     //获得作图区域的DOM元素
     threeArea = document.getElementById("three-area");
@@ -106,44 +135,28 @@ function init () {
     scene = new THREE.Scene();
 
     //创建摄像机
-    camera = new THREE.PerspectiveCamera(45, threeArea.clientWidth / threeArea.clientHeight, 0.1, 1000);
-    camera.position.x = 0;
-    camera.position.y = 100;
-    camera.position.z = 100;
+    camera = createCamera();
     camera.lookAt(scene.position);
-    //camera.lookAt(0, 0, 0);
 
-    //创建WebGL渲染器
-    render = new THREE.WebGLRenderer();
-    render.setClearColor(0xf0f0f0);
-
-    render.gammaInput = true;
-    render.gammaOutput = true;
-    render.shadowMap.enabled = true;
-    render.shadowMapType = THREE.PCFSoftShadowMap;
+    //创建渲染器
+    render = createRender();
 
     //添加各种对象
     addGeometry(scene);
 
-    asciiRender = new AsciiEffect(render);
-
     //添加到页面以及渲染各种事件响应
     threeArea.appendChild(render.domElement);
-    //threeArea.appendChild(asciiRender.domElement);
 
     //配置轨道控制器
     let orbitControls = new OrbitControls(camera, render.domElement);
-    //let orbitControls = new OrbitControls(camera, asciiRender.domElement);
     orbitControls.enableDamping = true;
     orbitControls.dampingFactor = 0.25;
     orbitControls.enableZoom = true;
     orbitControls.autoRotate = true;
 
-    render.setSize(threeArea.clientWidth, threeArea.clientHeight);
-    //asciiRender.setSize(threeArea.clientWidth, threeArea.clientHeight);
+
     
     render.render(scene, camera);
-    //asciiRender.render(scene, camera);
     window.addEventListener('resize', onWindowResize, false);
     animate();
 }
@@ -154,29 +167,17 @@ function onWindowResize() {
     let height = threeArea.clientHeight;
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
-    
     render.setSize(width, height);
-    //asciiRender.setSize(width, height);
 }
-
-let step = 0;
 
 //响应更新画面的函数
 function animate() {
     stats.update();
 
-    cube.rotation.x += controls.rotationSpeed;
-    cube.rotation.y += controls.rotationSpeed;
-    cube.rotation.z += controls.rotationSpeed;
-    
-    step += controls.bouncingSpeed;
-    sphere.position.x = 30 * Math.cos(step);
-    sphere.position.y = 10 + Math.abs(20 * Math.sin(step));
-    
-    requestAnimationFrame(animate);
+    spotLight.position.set(controls.spotX, controls.spotY, controls.spotZ);
 
+    requestAnimationFrame(animate);
     render.render(scene, camera);
-    //asciiRender.render(scene, camera);
 }
 
 window.onload = init;
